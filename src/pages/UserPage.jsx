28bd4e1d301c76edbe5fe898a68d6c53f1d25a51
tamsoul <tamsoul>
2024-08-27 18:51:33 +0700

    add userPost

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,11 @@
 import UserHeader from '../components/UserHeader/UserHeader';
+import UserPost from '../components/UserPost/UserPost';
 
 const UserPage = () => {
     return (
         <div>
             <UserHeader />
+            <UserPost />
         </div>
     );
 };
