acbe3c108f62e471b0bed513314e7e987d532059
SoulMinT05 <minhtam5111993@gmail.com>
2024-09-19 21:23:36 +0700

    add getFriendsByUser, unfriend and add follow when add friend, delete follow when reject or unfriend

--- controllers/FriendController.js
+++ controllers/FriendController.js
@@ -13,7 +13,42 @@ const addFriendRequest = asyncHandler(async (req, res) => {
             { requester: recipientId, recipient: requesterId },
         ],
     });
-    if (existingFriendship) throw new Error('Friend request already exists or you are already friends.');
+
+    // status: pending, or rejected
+    if (existingFriendship) {
+        // Nếu yêu cầu đã bị từ chối trước đó, cho phép gửi lại
+        if (existingFriendship.status === 'rejected') {
+            existingFriendship.status = 'pending';
+            await existingFriendship.save();
+
+            await User.findByIdAndUpdate(
+                requesterId,
+                { $addToSet: { following: recipientId } }, // $addToSet để đảm bảo không trùng
+                { new: true },
+            );
+
+            await User.findByIdAndUpdate(
+                recipientId,
+                { $addToSet: { followers: requesterId } }, // $addToSet để đảm bảo không trùng
+                { new: true },
+            );
+            const updatedFriendship = await Friend.findById(existingFriendship._id)
+                .populate('requester', 'name username avatar')
+                .populate('recipient', 'name username avatar')
+                .exec();
+
+            if (updatedFriendship.status !== 'pending') {
+                throw new Error('Friend request already exists or you are already friends.');
+            }
+
+            return res.status(200).json({
+                message: 'Friend request resent successfully',
+                updatedFriendship,
+            });
+        }
+        // Nếu yêu cầu vẫn đang chờ hoặc đã được chấp nhận, không cho phép gửi lại
+        throw new Error('Friend request already exists or you are already friends.');
+    }
 
     const newFriendship = await Friend.create({
         requester: requesterId,
@@ -24,7 +59,7 @@ const addFriendRequest = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         requesterId,
         {
-            $push: { friends: newFriendship._id },
+            $push: { friends: newFriendship._id, following: recipientId },
         },
         { new: true },
     );
@@ -32,7 +67,7 @@ const addFriendRequest = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         recipientId,
         {
-            $push: { friends: newFriendship._id },
+            $push: { friends: newFriendship._id, followers: requesterId },
         },
         { new: true },
     );
@@ -40,22 +75,6 @@ const addFriendRequest = asyncHandler(async (req, res) => {
     const populatedFriendship = await Friend.findById(newFriendship._id)
         .populate('requester', 'name username avatar')
         .populate('recipient', 'name username avatar');
-
-    // const populatedRequester = await User.findById(requesterId).populate({
-    //     path: 'friends',
-    //     populate: [
-    //         { path: 'requester', select: 'name username avatar' },
-    //         { path: 'recipient', select: 'name username avatar' },
-    //     ],
-    // });
-
-    // const populatedRecipient = await User.findById(recipientId).populate({
-    //     path: 'friends',
-    //     populate: [
-    //         { path: 'requester', select: 'name username avatar' },
-    //         { path: 'recipient', select: 'name username avatar' },
-    //     ],
-    // });
 
     return res.status(200).json({
         message: populatedFriendship ? 'Send request add friend successfully' : 'Send request add friend failed',
@@ -102,6 +121,18 @@ const rejectFriendRequest = asyncHandler(async (req, res) => {
     friendRequest.status = 'rejected';
     await friendRequest.save();
 
+    await User.findByIdAndUpdate(
+        friendRequest.requester,
+        { $pull: { following: friendRequest.recipient } },
+        { new: true },
+    );
+
+    await User.findByIdAndUpdate(
+        friendRequest.recipient,
+        { $pull: { followers: friendRequest.requester } },
+        { new: true },
+    );
+
     res.status(200).json({
         success: friendRequest ? true : false,
         message: friendRequest ? 'Reject friend successfully' : 'Reject friend failed',
@@ -109,8 +140,66 @@ const rejectFriendRequest = asyncHandler(async (req, res) => {
     });
 });
 
+const getFriendsByUser = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) throw new Error('User not found');
+
+    const userFriend = await User.findById(userId).populate({
+        path: 'friends',
+        match: { status: 'accepted' },
+        populate: [
+            { path: 'requester', select: '-refreshToken -password -role -isAdmin -createdAt -updatedAt -isBlocked' },
+            { path: 'recipient', select: '-refreshToken -password -role -isAdmin -createdAt -updatedAt -isBlocked' },
+        ],
+    });
+
+    res.status(200).json({ success: true, message: 'Get friends by user successfully', userFriend });
+});
+
+const unfriend = asyncHandler(async (req, res) => {
+    const { friendId } = req.params;
+    const userId = req.user._id;
+
+    const friendShip = await Friend.findOne({
+        $or: [
+            { requester: userId, recipient: friendId, status: 'accepted' },
+            { requester: friendId, recipient: userId, status: 'accepted' },
+        ],
+    });
+    if (!friendShip) throw new Error('Friendship not found or already unfriended');
+    await Friend.findByIdAndDelete(friendShip._id);
+
+    await User.findByIdAndUpdate(
+        friendId,
+        {
+            $pull: { friends: friendShip._id },
+        },
+        { new: true },
+    );
+
+    await User.findByIdAndUpdate(
+        userId,
+        {
+            $pull: { friends: friendShip._id },
+        },
+        { new: true },
+    );
+
+    await User.findByIdAndUpdate(friendId, { $pull: { followers: userId, following: userId } }, { new: true });
+    await User.findByIdAndUpdate(userId, { $pull: { followers: friendId, following: friendId } }, { new: true });
+
+    return res.status(200).json({
+        success: true,
+        message: 'Unfriend successfully',
+        friendShip,
+    });
+});
+
 module.exports = {
     addFriendRequest,
     acceptFriendRequest,
     rejectFriendRequest,
+    getFriendsByUser,
+    unfriend,
 };
--- routes/FriendRouter.js
+++ routes/FriendRouter.js
@@ -6,6 +6,8 @@ router.post('/addFriend/:userId', verifyAccessToken, FriendController.addFriendR
 router.post('/acceptFriend/:requestId', verifyAccessToken, FriendController.acceptFriendRequest);
 router.post('/rejectFriend/:requestId', verifyAccessToken, FriendController.rejectFriendRequest);
 
-// router.get('/:userId/friends', FriendController.getFriends);
+router.delete('/unfriend/:friendId', verifyAccessToken, FriendController.unfriend);
+
+router.get('/getFriendsByUser/:userId', verifyAccessToken, FriendController.getFriendsByUser);
 
 module.exports = router;
